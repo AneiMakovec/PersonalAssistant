126102662903fc943b0e710a41e511ec7ff077ad
GoodMeat <sk4919@student.uni-lj.si>
2017-12-30 19:45:01 +0100

    Dodal: KategorijeActivity, dodelal kalkulator

--- app/src/main/java/com/sminarska/emp/personalassistant/DodajActivity.java
+++ app/src/main/java/com/sminarska/emp/personalassistant/DodajActivity.java
@@ -1,6 +1,7 @@
 package com.sminarska.emp.personalassistant;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -8,6 +9,11 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class DodajActivity extends AppCompatActivity {
+
+    public boolean operatorPressed = false;
+    public double st1;
+    public double st2;
+    public String OP;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +39,16 @@ public class DodajActivity extends AppCompatActivity {
         Button brisi = (Button)findViewById(R.id.brisi);
         Button izberiKat = (Button)findViewById(R.id.izberiKat);
         final TextView rezultat = (TextView)findViewById(R.id.rezultat);
+
+        izberiKat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(DodajActivity.this, KategorijeActivity.class);
+                intent.putExtra("testKategorija", "to je test3");
+                startActivity(intent);
+            }
+        });
+
 
         ena.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -103,27 +119,74 @@ public class DodajActivity extends AppCompatActivity {
         krat.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                rezultat.setText(rezultat.getText() + "*");
+                if(!operatorPressed) {
+                    operatorPressed = true;
+                    OP = "×";
+                    rezultat.setText(rezultat.getText() + "×");
+                }
             }
         });
         deljeno.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                rezultat.setText(rezultat.getText() + "/");
+                if(!operatorPressed) {
+                    operatorPressed = true;
+                    OP = "/";
+                    rezultat.setText(rezultat.getText() + "/");
+                }
             }
         });
         plus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                rezultat.setText(rezultat.getText() + "+");
+                if(!operatorPressed) {
+                    operatorPressed = true;
+                    OP = "\\+";
+                    rezultat.setText(rezultat.getText() + "+");
+                }
             }
         });
         minus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                rezultat.setText(rezultat.getText() + "-");
+                if(!operatorPressed) {
+                    operatorPressed = true;
+                    OP = "-";
+                    rezultat.setText(rezultat.getText() + "-");
+                }
             }
         });
+        enako.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                operatorPressed = false;
+                double rez = 0.0;
+                String temp = (String)rezultat.getText();
+                String[] st = temp.split(OP);
+                st1 = Double.parseDouble(st[0]);
+                st2 = Double.parseDouble(st[1]);
+                switch(OP) {
+                    case "\\+":
+                        rez = st1 + st2;
+                        break;
+                    case "-":
+                        rez = st1 - st2;
+                        break;
+                    case "×":
+                        rez = st1 * st2;
+                        break;
+                    case "/":
+                        rez = st1 / st2;
+                        break;
+                }
+
+                rezultat.setText(String.format( "%.2f", rez));
+
+            }
+        });
+
+
+
 
     }
 }
--- app/src/main/java/com/sminarska/emp/personalassistant/ExpandableListAdapter.java
+++ app/src/main/java/com/sminarska/emp/personalassistant/ExpandableListAdapter.java
@@ -21,8 +21,7 @@ public class ExpandableListAdapter extends BaseExpandableListAdapter {
     // child data in format of header title, child title
     private HashMap<String, List<String>> _listDataChild;
 
-    public ExpandableListAdapter(Context context, List<String> listDataHeader,
-                                 HashMap<String, List<String>> listChildData) {
+    public ExpandableListAdapter(Context context, List<String> listDataHeader, HashMap<String, List<String>> listChildData) {
         this._context = context;
         this._listDataHeader = listDataHeader;
         this._listDataChild = listChildData;
@@ -39,28 +38,20 @@ public class ExpandableListAdapter extends BaseExpandableListAdapter {
     }
 
     @Override
-    public View getChildView(int groupPosition, final int childPosition,
-                             boolean isLastChild, View convertView, ViewGroup parent) {
-
+    public View getChildView(int groupPosition, final int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
         final String childText = (String) getChild(groupPosition, childPosition);
-
         if (convertView == null) {
-            LayoutInflater infalInflater = (LayoutInflater) this._context
-                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            LayoutInflater infalInflater = (LayoutInflater) this._context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             convertView = infalInflater.inflate(R.layout.list_item, null);
         }
-
-        TextView txtListChild = (TextView) convertView
-                .findViewById(R.id.lblListItem);
-
+        TextView txtListChild = (TextView) convertView.findViewById(R.id.lblListItem);
         txtListChild.setText(childText);
         return convertView;
     }
 
     @Override
     public int getChildrenCount(int groupPosition) {
-        return this._listDataChild.get(this._listDataHeader.get(groupPosition))
-                .size();
+        return this._listDataChild.get(this._listDataHeader.get(groupPosition)).size();
     }
 
     @Override
@@ -79,17 +70,13 @@ public class ExpandableListAdapter extends BaseExpandableListAdapter {
     }
 
     @Override
-    public View getGroupView(int groupPosition, boolean isExpanded,
-                             View convertView, ViewGroup parent) {
+    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
         String headerTitle = (String) getGroup(groupPosition);
         if (convertView == null) {
-            LayoutInflater infalInflater = (LayoutInflater) this._context
-                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            LayoutInflater infalInflater = (LayoutInflater) this._context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             convertView = infalInflater.inflate(R.layout.list_group, null);
         }
-
-        TextView lblListHeader = (TextView) convertView
-                .findViewById(R.id.lblListHeader);
+        TextView lblListHeader = (TextView) convertView.findViewById(R.id.lblListHeader);
         lblListHeader.setTypeface(null, Typeface.BOLD);
         lblListHeader.setText(headerTitle);
 
