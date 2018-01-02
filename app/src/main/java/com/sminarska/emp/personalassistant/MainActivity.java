69d7d4432c941891fa22e0f2cc946243b8ce9f2a
AneiMakovec <anei.makovec@hotmail.com>
2018-01-02 19:02:48 +0100

    Finished design for navigation drawer in main activity (not working).

--- app/src/main/java/com/sminarska/emp/personalassistant/MainActivity.java
+++ app/src/main/java/com/sminarska/emp/personalassistant/MainActivity.java
@@ -1,14 +1,11 @@
 package com.sminarska.emp.personalassistant;
 
 import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import android.app.Activity;
-import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ExpandableListView;
@@ -16,8 +13,12 @@ import android.widget.ExpandableListView.OnChildClickListener;
 import android.widget.ExpandableListView.OnGroupClickListener;
 import android.widget.ExpandableListView.OnGroupCollapseListener;
 import android.widget.ExpandableListView.OnGroupExpandListener;
-import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     // expandable list
@@ -27,9 +28,8 @@ public class MainActivity extends AppCompatActivity {
     HashMap<String, List<String>> listDataChild;
 
     // navigation drawer
-    List<String> drawerData;
     DrawerLayout drawerLayout;
-    ListView listView;
+    NavigationView drawerView;
 
 
     @Override
@@ -57,6 +57,8 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        // PREPARE THE EXPANDABLE LIST
 
         // get the listview
         expListView = (ExpandableListView) findViewById(R.id.lvExp);
@@ -105,6 +107,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // TEGA NE BOVA RABLA
         // Listview on child click listener
         expListView.setOnChildClickListener(new OnChildClickListener() {
 
@@ -116,6 +119,28 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
+
+
+
+        // PREPARE THE NAVIGATION DRAWER
+
+        // get the view
+        drawerView = (NavigationView) findViewById(R.id.drawer_view);
+
+        // get the layout
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        // add item activated listener
+        drawerView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        menuItem.setChecked(true);
+                        drawerLayout.closeDrawers();
+                        return true;
+                    }
+                });
+
     }
 
     /*
@@ -125,87 +150,69 @@ public class MainActivity extends AppCompatActivity {
         listDataHeader = new ArrayList<String>();
         listDataChild = new HashMap<String, List<String>>();
 
-        // Adding child data
+        // Adding parent data
         listDataHeader.add("Plača");
         listDataHeader.add("Potni stroški");
         listDataHeader.add("Hrana");
         listDataHeader.add("Obleke");
+        listDataHeader.add("Hišni stroški");
         listDataHeader.add("Igrače");
         listDataHeader.add("Darila");
         listDataHeader.add("Prosti čas");
-        listDataHeader.add("Muzika");
 
         // Adding child data
-        List<String> top250 = new ArrayList<String>();
-        top250.add("Bruto");
-        top250.add("Neto");
+        List<String> placa = new ArrayList<String>();
+        placa.add("Bruto");
+        placa.add("Neto");
 
-        List<String> nowShowing = new ArrayList<String>();
-        nowShowing.add("The Conjuring");
-        nowShowing.add("Despicable Me 2");
-        nowShowing.add("Turbo");
-        nowShowing.add("Grown Ups 2");
-        nowShowing.add("Red 2");
-        nowShowing.add("The Wolverine");
+        List<String> potStroski = new ArrayList<String>();
+        potStroski.add("Gorivo");
+        potStroski.add("Stroški za avto");
 
-        List<String> comingSoon = new ArrayList<String>();
-        comingSoon.add("2 Guns");
-        comingSoon.add("The Smurfs 2");
-        comingSoon.add("The Spectacular Now");
-        comingSoon.add("The Canyons");
-        comingSoon.add("Europa Report");
+        List<String> hrana = new ArrayList<String>();
+        hrana.add("Zelenjava");
+        hrana.add("Mesni izdelki");
+        hrana.add("Izdelki iz žit");
+        hrana.add("Sadje");
+        hrana.add("Pijača");
+        hrana.add("Drugo");
 
-        List<String> t1 = new ArrayList<String>();
-        t1.add("2 Guns");
-        t1.add("The Smurfs 2");
-        t1.add("The Spectacular Now");
-        t1.add("The Canyons");
-        t1.add("Europa Report");
+        List<String> obleke = new ArrayList<String>();
+        obleke.add("Zgornji del");
+        obleke.add("Spodnji del");
+        obleke.add("Obutev");
+        obleke.add("Dodatki");
 
-        List<String> t2 = new ArrayList<String>();
-        t2.add("2 Guns");
-        t2.add("The Smurfs 2");
-        t2.add("The Spectacular Now");
-        t2.add("The Canyons");
-        t2.add("Europa Report");
+        List<String> hisniStroski = new ArrayList<String>();
+        hisniStroski.add("Voda");
+        hisniStroski.add("Elektrika");
+        hisniStroski.add("Televizija/Internet");
+        hisniStroski.add("Ogrevanje");
+        hisniStroski.add("Drugo");
 
-        List<String> t3 = new ArrayList<String>();
-        t3.add("2 Guns");
-        t3.add("The Smurfs 2");
-        t3.add("The Spectacular Now");
-        t3.add("The Canyons");
-        t3.add("Europa Report");
+        List<String> igrace = new ArrayList<String>();
+        igrace.add("Velike");
+        igrace.add("Majhne");
+        igrace.add("Drugo");
 
-        List<String> t4 = new ArrayList<String>();
-        t4.add("2 Guns");
-        t4.add("The Smurfs 2");
-        t4.add("The Spectacular Now");
-        t4.add("The Canyons");
-        t4.add("Europa Report");
+        List<String> darila = new ArrayList<String>();
+        darila.add("Prazniki");
+        darila.add("Rojstni dnevi");
+        darila.add("Drugo");
 
-        List<String> t5 = new ArrayList<String>();
-        t5.add("2 Guns");
-        t5.add("The Smurfs 2");
-        t5.add("The Spectacular Now");
-        t5.add("The Canyons");
-        t5.add("Europa Report");
+        List<String> prostiCas = new ArrayList<String>();
+        prostiCas.add("Hobiji");
+        prostiCas.add("Šport");
+        prostiCas.add("Glasbila");
+        prostiCas.add("Drugo");
 
-        listDataChild.put(listDataHeader.get(0), top250); // Header, Child data
-        listDataChild.put(listDataHeader.get(1), nowShowing);
-        listDataChild.put(listDataHeader.get(2), comingSoon);
-        listDataChild.put(listDataHeader.get(3), t1);
-        listDataChild.put(listDataHeader.get(4), t2);
-        listDataChild.put(listDataHeader.get(5), t3);
-        listDataChild.put(listDataHeader.get(6), t4);
-        listDataChild.put(listDataHeader.get(7), t5);
-    }
-
-    /*
-        Preparing the navigation drawer data
-     */
-    private void prepareNavigationDrawerData() {
-        drawerData = new ArrayList<String>();
-
-        // TODO
+        listDataChild.put(listDataHeader.get(0), placa); // Header, Child data
+        listDataChild.put(listDataHeader.get(1), potStroski);
+        listDataChild.put(listDataHeader.get(2), hrana);
+        listDataChild.put(listDataHeader.get(3), obleke);
+        listDataChild.put(listDataHeader.get(4), hisniStroski);
+        listDataChild.put(listDataHeader.get(5), igrace);
+        listDataChild.put(listDataHeader.get(6), darila);
+        listDataChild.put(listDataHeader.get(7), prostiCas);
     }
 }
